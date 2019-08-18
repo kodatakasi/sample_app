4b366d7bc81d3f68ae139d4199228b1543de66e0
koda takasi <kodatakasi@gmail.com>
2019-08-19 06:48:39 +0900

    Add pasword reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,9 +18,18 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://protected-cove-69705.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
   config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
